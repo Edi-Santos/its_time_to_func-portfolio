e6f3b2944f5c991b63722e4d961bc9fd681e390c
Ed Santos <edsantos2822@gmail.com>
2022-06-25 19:00:54 -0300

    buidl: concluindo desafio 9

--- others_challanges.js
+++ others_challanges.js
@@ -9,3 +9,16 @@ function alternativeCalculator(sign, n1, n2) {
 
   return operations[sign];
 }
+
+// Desafio 9 - Escraba uma função que imprima um quadrado de "*"
+function asteriskSquare(n) {
+  let asterisk = "";
+
+  for (let i = 0; i < n; i += 1) {
+    for (let ii = 0; ii < n; ii += 1) {
+      asterisk += "*";
+    }
+    console.log(asterisk);
+    asterisk = "";
+  }
+}
