63a3b6019749d677557f01f17fa06d6150fc32e9
Ed Santos <edsantos2822@gmail.com>
2022-06-27 01:03:58 -0300

    test:(challanges-1.test.js): adicionando testes para a função conversionTempMeasur()

--- tests/challanges-1.test.js
+++ tests/challanges-1.test.js
@@ -33,4 +33,30 @@ describe('Testando arquivo challanges-1.js', () => {
     assert.deepStrictEqual(teste3, expected3);
     assert.deepStrictEqual(teste4, expected4);
   });
+
+  it('função conversionTempMeasur()', () => {
+    const teste1 = challanges.conversionTempMeasur(30, 'celsius', 'kelvin');
+    const teste2 = challanges.conversionTempMeasur(30, 'celsius', 'fahrenheit');
+    const teste3 = challanges.conversionTempMeasur(292, 'kelvin', 'celsius');
+    const teste4 = challanges.conversionTempMeasur(292, 'kelvin', 'fahrenheit');
+    const teste5 = challanges.conversionTempMeasur(292, 'fahrenheit', 'celsius');
+    const teste6 = challanges.conversionTempMeasur(292, 'fahrenheit', 'kelvin');
+    const teste7 = challanges.conversionTempMeasur(0, "", "");
+
+    const expected1 = 303;
+    const expected2 = 86;
+    const expected3 = 19;
+    const expected4 = 66.2;
+    const expected5 = 144.44444444444446;
+    const expected6 = 417.44444444444446;
+    const expected7 = "Dados incorrentos.";
+
+    assert.deepStrictEqual(teste1, expected1);
+    assert.deepStrictEqual(teste2, expected2);
+    assert.deepStrictEqual(teste3, expected3);
+    assert.deepStrictEqual(teste4, expected4);
+    assert.deepStrictEqual(teste5, expected5);
+    assert.deepStrictEqual(teste6, expected6);
+    assert.deepStrictEqual(teste7, expected7);
+  });
 });
