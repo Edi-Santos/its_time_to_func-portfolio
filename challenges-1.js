4d74d8e4aa6749b5115f27f460a9e307d3711a82
Ed Santos <edsantos2822@gmail.com>
2022-06-29 17:02:34 -0300

    style: removendo linha em branco

--- challenges-1.js
+++ challenges-1.js
@@ -3,7 +3,6 @@
 
 // Desafio 1 - Escreva uma função simples que diga se um aluno está aprovado, em recuperação ou reprovado com base na nota que a função receberá por parâmetro.
 // Reprovado: 0 a 4,9; Recuperação: 5 a 7; Aprovado: 7,1 a 10.
-
 function studentStats(grade) {
   if (grade < 0) return 'Nota inválida.';
   if (grade >= 0 && grade < 5) return 'Aluno reprovado.';
