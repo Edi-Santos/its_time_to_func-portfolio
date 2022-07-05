6176ef9331e48ff6dce55821fe1fda3edf8403a3
Ed Santos <edsantos2822@gmail.com>
2022-07-05 13:33:11 -0300

    build: adicionando diretório base_answer

---
+++ base_answer/challenges-1.js
@@ -0,0 +1,71 @@
+// Desafios de estruturas condicionais (if/else e switch/case).
+// Fique a vontade para usar o que achar mais conveniente.
+
+// Desafio 1 - Escreva uma função simples que diga se um aluno está aprovado, em recuperação ou reprovado com base na nota que a função receberá por parâmetro.
+// Reprovado: 0 a 4,9; Recuperação: 5 a 7; Aprovado: 7,1 a 10.
+function studentStats(grade) {
+  if (grade < 0) return 'Nota inválida.';
+  if (grade >= 0 && grade < 5) return 'Aluno reprovado.';
+  if (grade <= 7) return 'Aluno em recuperação';
+  if (grade >= 7.1 && grade <= 10) return 'Aluno aprovado';
+
+  return 'Nota inválida.';
+}
+
+// Desafio 2 - Escreva uma função que calcula as 4 operações fundamentais.
+function calculator(sign, n1, n2) {
+  if (sign === '+') return n1 + n2;
+  if (sign === '-') return n1 - n2;
+  if (sign === '*') return n1 * n2;
+  if (sign === '/') return n1 / n2;
+
+  return 'Sinal inválido.';
+}
+
+// Desafio 3 - Escreva uma função que faça a conversão de medidas de temperatura: Celsius, Kelvin e Fahrenheit
+function conversionTempMeasur(degrees, currentMeasure, conversion) {
+  const currToLowerCase = currentMeasure.toLowerCase();
+  const converToLowerCase = conversion.toLowerCase();
+  
+  if (currToLowerCase === 'celsius' && converToLowerCase === 'kelvin') {
+    return degrees + 273;
+  }
+
+  if (currToLowerCase === 'kelvin' && converToLowerCase === 'celsius') {
+    return degrees - 273;
+  }
+
+  if (currToLowerCase === 'celsius' && converToLowerCase === 'fahrenheit') {
+    const calcToFahrenheit = ((9 * degrees) + (5 * 32)) / 5;
+
+    return calcToFahrenheit;
+  }
+
+  if (currToLowerCase === 'kelvin' && converToLowerCase === 'fahrenheit') {
+    const kelvinToCelsius = degrees - 273;
+    const calcToFahrenheit = ((9 * kelvinToCelsius) + (5 * 32)) / 5;
+    
+    return calcToFahrenheit;
+  }
+
+  if (currToLowerCase === 'fahrenheit' && converToLowerCase === 'celsius') {
+    const calcToCelsius = (5 * (degrees - 32)) / 9;
+
+    return calcToCelsius;
+  }
+
+  if (currToLowerCase === 'fahrenheit' && converToLowerCase === 'kelvin') {
+    const calcToCelsius = (5 * (degrees - 32)) / 9;
+    const calcToKelvin = calcToCelsius + 273;
+
+    return calcToKelvin;
+  }
+
+  return 'Dados incorrentos.';
+}
+
+module.exports = {
+  studentStats,
+  calculator,
+  conversionTempMeasur,
+}
---
+++ base_answer/others_challenges.js
@@ -0,0 +1,80 @@
+// Desafio 8 - Escreva uma função que cumpra o Desafio 2 sem usar if/else ou switch/case.
+function alternativeCalculator(sign, n1, n2) {
+  const operations = {
+    '+': n1 + n2,
+    '-': n1 - n2,
+    '*': n1 * n2,
+    '/': n1 / n2,
+  };
+
+  const operation = operations[sign];
+
+  return operation ? operation : 'Sinal inválido.';
+}
+
+// Desafio 9 - Escraba uma função que imprima um quadrado de "*"
+function asteriskSquare(n) {
+  let asterisk = "";
+
+  for (let i = 0; i < n; i += 1) {
+    for (let ii = 0; ii < n; ii += 1) {
+      asterisk += "*";
+    }
+    console.log(asterisk);
+    asterisk = "";
+  }
+}
+
+// Desafio 10 - Escreva uma função que imprima um triângulo de "*" começando com 1 asterísco
+//e terminando com n asteríscos
+function asteriskTriangle(n) {
+  let asterisk = "";
+
+  for (let i = 0; i < n; i += 1) {
+    asterisk += "*";
+    console.log(asterisk);
+  }
+}
+
+// Desafio 11 - Escreva uma função que imprima um triângulo de "*" começando com n asteríscos e
+// terminando com 1 asterísco
+function inverseAsteriskTriangle(n) {
+  let arrayNum = [];
+  let asterisk = "";
+
+  for (let i = 1; i <= n; i += 1) {
+    arrayNum.push(i);
+  }
+
+  for (let i = 0; i < arrayNum.length; i = 0) {
+    for (let ii = arrayNum.length; ii > 0; ii -= 1) {
+      asterisk += "*";
+    }
+    console.log(asterisk);
+    arrayNum.pop();
+    asterisk = "";
+  }
+}
+
+// Desafio 12 - Escreva um função que diga se o parâmetro recebido é um palíndromo
+function isPalindrome(sentence) {
+  let sentenceRightToLeft = "";
+
+  for (let i = (sentence.length - 1); i >= 0; i -= 1) {
+    sentenceRightToLeft += sentence[i];
+  }
+
+  if (sentence === sentenceRightToLeft) {
+    return true;
+  }
+
+  return false;
+}
+
+module.exports = {
+  alternativeCalculator,
+  asteriskSquare,
+  asteriskTriangle,
+  inverseAsteriskTriangle,
+  isPalindrome,
+};
